9af482fb407841d78cc74a66310d3f31f6dc8f34
Rospaccio <alberto.mercati@gmail.com>
2016-03-08 21:06:32 +0100

    momentarily removed cache from images task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -62,13 +62,13 @@ gulp.task('html', ['styles', 'scripts'], () => {
 
 gulp.task('images', () => {
   return gulp.src('app/images/**/*')
-    .pipe($.cache($.imagemin({
-      progressive: true,
-      interlaced: true,
-      // don't remove IDs from SVGs, they are often used
-      // as hooks for embedding and styling
-      svgoPlugins: [{cleanupIDs: false}]
-    })))
+//    .pipe($.cache($.imagemin({
+//      progressive: true,
+//      interlaced: true,
+//      // don't remove IDs from SVGs, they are often used
+//      // as hooks for embedding and styling
+//      svgoPlugins: [{cleanupIDs: false}]
+//    })))
     .pipe(gulp.dest('dist/images'));
 });
 
