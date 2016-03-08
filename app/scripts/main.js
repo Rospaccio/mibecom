ab43bee53c5635fe66de7afbabc54cd3589ebe50
Rospaccio <alberto.mercati@gmail.com>
2016-03-08 19:46:29 +0100

    redirection to preferred hostname; loading of the post specified in the url;

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,7 +7,7 @@ Codevomit.Blog.App.init = function(){
 
   $(document).ready(function () {
     $('[data-toggle="offcanvas"]').click(function () {
-      $('.row-offcanvas').toggleClass('active')
+      $('.row-offcanvas').toggleClass('active');
     });
   });
 
@@ -24,15 +24,12 @@ Codevomit.Blog.App.init = function(){
 
   var container = $("#postsIndex");
 
+  /* gets the configuration and sets the post links up*/
   $.get("conf.json", function(data){
-
     Codevomit.Blog.App.Configuration = data;
-
     data.posts.sort(Codevomit.Blog.App.postCompare);
-
     data.posts.forEach(function(post){
       var newItem = document.createElement("li");
-
       var newItem = document.createElement("li");
       var anchorToPost = newItem.appendChild( document.createElement("a") );
       var jQAnchor = $(anchorToPost);
@@ -41,13 +38,41 @@ Codevomit.Blog.App.init = function(){
         Codevomit.Blog.App.showPost(prefix + post.url);
       });
       jQAnchor.html(post.indexTitle);
-
       container.append(newItem)
     });
-
-    Codevomit.Blog.App.showPost(Codevomit.Blog.App.Configuration.postsSiteUrl + data.posts[0].url);
-
+    Codevomit.Blog.App.checkHostAndPossiblyRedirect();
+    if(!Codevomit.Blog.App.checkIfPostSpecified()){
+      /* no post specified in the URL, so we load the default one*/
+      Codevomit.Blog.App.showPost(Codevomit.Blog.App.Configuration.postsSiteUrl + data.posts[0].url);
+    }
   });
+  /* end configuration */
+}; /* end init */
+
+Codevomit.Blog.App.checkIfPostSpecified = function(){
+  var currentURL = window.location.href;
+  console.log("current URL = " + currentURL);
+  var isBookmarkPresent = currentURL.indexOf("#") > 0;
+  if(isBookmarkPresent){
+    var lastSeparator = currentURL.lastIndexOf("/");
+    var postRelativeURL = currentURL.substring(lastSeparator);
+    Codevomit.Blog.App.showPost(Codevomit.Blog.App.Configuration.postsSiteUrl + postRelativeURL);
+    return true;
+  }
+  return false;
+};
+
+Codevomit.Blog.App.checkHostAndPossiblyRedirect = function(){
+  console.log("Codevomit.Blog.App.Configuration.preferredHost = " + Codevomit.Blog.App.Configuration.preferredHost);
+  if(!window.location.host.startsWith(Codevomit.Blog.App.Configuration.preferredHost))
+  {
+    console.log("Trying to redirect...");
+    var currentHost = window.location.host;
+    console.log("currentHost = " + currentHost);
+    var preferredHref = window.location.href.replace(currentHost, Codevomit.Blog.App.Configuration.preferredHost);
+    console.log("preferredHref = " + preferredHref);
+    window.location.href = preferredHref;
+  }
 };
 
 Codevomit.Blog.App.showPost = function(postUrl){
@@ -60,6 +85,10 @@ Codevomit.Blog.App.showPost = function(postUrl){
     codeBlocks.each(function(i, block){
       hljs.highlightBlock(block);
     });
+
+    /* let's update the history and the url bar appropriately */
+    window.history.pushState({}, {}, "#" + postUrl);
+
   });
 };
 
